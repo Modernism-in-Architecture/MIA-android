81ac0786de905e605bdfca2d31c758952410a3a6
Norma Driske <hello@normade.dev>
2022-05-18 22:12:42 +0200

    Add layout basics

--- app/src/main/java/org/modernism_in_architecture/mia/MainActivity.kt
+++ app/src/main/java/org/modernism_in_architecture/mia/MainActivity.kt
@@ -2,13 +2,37 @@ package org.modernism_in_architecture.mia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buildingFragment = BuildingFragment()
+        val placesFragment = PlacesFragment()
+        val architectsFragment = ArchitectsFragment()
+
+        setCurrentFragment(buildingFragment)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigation.setOnItemSelectedListener  {
+            when(it.itemId){
+                R.id.buildings->setCurrentFragment(buildingFragment)
+                R.id.places->setCurrentFragment(placesFragment)
+                R.id.architects->setCurrentFragment(architectsFragment)
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
     }
 
 
