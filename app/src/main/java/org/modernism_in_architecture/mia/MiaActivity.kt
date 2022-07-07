2477077211f10b6b011e82d6aa53ac35383a185a
Norma Driske <hello@normade.dev>
2022-07-07 17:08:13 +0200

    Prepare search bar

--- app/src/main/java/org/modernism_in_architecture/mia/MiaActivity.kt
+++ app/src/main/java/org/modernism_in_architecture/mia/MiaActivity.kt
@@ -1,7 +1,11 @@
 package org.modernism_in_architecture.mia
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -10,8 +14,6 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MiaActivity : AppCompatActivity(R.layout.activity_mia) {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +27,24 @@ class MiaActivity : AppCompatActivity(R.layout.activity_mia) {
 
         setSupportActionBar(toolbar)
 
+        val actionBar = supportActionBar
+        actionBar?.setDisplayShowTitleEnabled(false)
+        actionBar?.setLogo(R.mipmap.mia_logo)
+        actionBar?.setDisplayUseLogoEnabled(true)
+
         // setupActionBarWithNavController(navController)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
+
+        return true
+    }
+
 }
 
